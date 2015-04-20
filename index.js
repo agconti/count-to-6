205932e8700ca584b96812ce5ba3a769c496643b
agconti <agc11d@gmail.com>
2015-04-20 14:22:06 -0400

    feat(index): solved arrow functions and this

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var inputs = process.argv.slice(2)
-  , result = inputs.map(item => item[0])
-                   .reduce((previousLetter, letter) => previousLetter.concat(letter))
-
-console.log(`[${ inputs }] becomes "${ result }"`)                          
+var foot = {
+  kick: function () {
+    this.yelp = "Ouch!"
+    setImmediate(() => console.log(this.yelp))
+  }
+}
+foot.kick()
