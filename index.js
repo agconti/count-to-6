24efbe9a028801e49d38629da2a2b7a380aa8978
agconti <agc11d@gmail.com>
2015-04-20 14:37:49 -0400

    feat(index): completed spread

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 var inputs = process.argv.slice(2)
-  , result = inputs.map(item => item[0])
-                   .reduce((previousLetter, letter) => previousLetter.concat(letter))
 
-console.log(`[${ inputs }] becomes "${ result }"`)                          
+console.log(`The minimum of [${inputs}] is ${Math.min(...inputs)}`)                          
