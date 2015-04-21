0396e4564c250d84bcaf5bf1e66549921d2f2414
agconti <agc11d@gmail.com>
2015-04-21 11:59:59 -0400

    feat(index) completed default arguments 2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var inputs = process.argv.slice(2)
-  , result = inputs.map(item => item[0])
-                   .reduce((previousLetter, letter) => previousLetter.concat(letter))
-
-console.log(`[${ inputs }] becomes "${ result }"`)                          
+module.exports = function makeImportant (message, exclamation=message.length) {
+  return `${message}${'!'.repeat(exclamation)}`
+}
